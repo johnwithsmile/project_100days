3c9290ffc59e778c9b516cd781ec03772c37282a
Jeongdong Kim <93374751+johnwithsmile@users.noreply.github.com>
2023-10-11 16:19:31 +0900

    basic js

---
+++ days/day30.js
@@ -0,0 +1,10 @@
+let greetingText;
+let age;
+let hobbies;
+let job;
+greetingText = "Hi my name is 'John'";
+age = 32;
+hobbies = ["Sports", "Cooking", "Reading"];
+job = { title: "Developer", place: "New York", salary: 50000 };
+
+// alert(hobbies[1]);
