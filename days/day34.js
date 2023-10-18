1de4aa2c8fbc7997d2f42f9b87c8c7a01c224d79
Jeongdong Kim <93374751+johnwithsmile@users.noreply.github.com>
2023-10-18 23:20:24 +0900

    js DOM control basics

---
+++ days/day34.js
@@ -0,0 +1,21 @@
+// console.log(window);
+
+// same
+// alert();
+// window.alert()
+
+// console.dir(window.document); // real object of DOM
+
+// document.body.children[1].children[0].href = "https://google.com";
+
+// document.body.firstChild;
+
+// document.body.firstElementChild
+// ​document.body.childNodes
+
+// let anchorElement = document.getElementById("external-link");
+
+// anchorElement.href = "https://google.com";
+
+// anchorElement = document.querySelector("p a");
+// anchorElement.href = "https://academind.com";
